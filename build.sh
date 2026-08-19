abed0c848e8dd1ac4bd6a5734858978feaf7a2be
Leonard Nielsen <leoniel@chalmers.se>
2026-08-19 12:52:09 +0000

    Add build script and xfs config file

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+export ELEMENTS_PATH="${ROOT}/elements"
+export DIB_BLOCK_DEVICE_CONFIG="file://${ROOT}/block-devices/xfs.yaml"
+
+cd "${ROOT}"
+
+diskimage-builder images/$1.yaml
