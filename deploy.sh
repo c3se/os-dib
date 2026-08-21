b1a31fcb9e9125bf35524d4f37af6a4a32a84081
Leonard Nielsen <leoniel@chalmers.se>
2026-08-21 13:25:12 +0000

    added simple deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,10 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${ROOT}"
 
-# Re-use same ID
-IMAGE_ID=$(openstack image show $1 -fvalue -cid)
+IMAGE_ID=$(openstack image show "$1" -fvalue -cid 2>/dev/null || echo "")
 
-.venv/bin/openstack image delete $IMAGE_ID 2>/dev/null || echo 1
+if [[ $IMAGE_ID != "" ]]; then
+  .venv/bin/openstack image delete "$IMAGE_ID"
+fi
 
-.venv/bin/openstack image create --id $IMAGE_ID --public --disk-format qcow2 --file $1.qcow2 $1
+.venv/bin/openstack image create --public --disk-format qcow2 --file "${1}".qcow2 "$1"
