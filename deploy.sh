3c7ac15b986e45ef0c6d4b23c1d8dc1639678ac1
Leonard Nielsen <leoniel@chalmers.se>
2026-08-24 09:37:43 +0000

    simplify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,9 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${ROOT}"
 
-IMAGE_ID=$(openstack image show "$1" -fvalue -cid 2>/dev/null || echo "")
+IMAGE_ID=$(openstack image show "$1" -fvalue -cid )
 
-if [[ $IMAGE_ID != "" ]]; then
-  .venv/bin/openstack image delete "$IMAGE_ID"
-fi
+.venv/bin/openstack image delete "$IMAGE_ID"
 
 .venv/bin/openstack image create --public --disk-format qcow2 --file "${1}".qcow2 "$1"
 
